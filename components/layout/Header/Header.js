a48cab2585e88e6d08216bedc4b36a6969349c21
Tushar <tusharkaramchandain25@gmail.com>
2022-04-13 21:55:46 +0530

    added changes

--- components/layout/Header/Header.js
+++ components/layout/Header/Header.js
@@ -9,6 +9,7 @@ const Header = () => {
     useContext(NftContext);
 
   const [userLoginPopup, setUserLoginPopup] = useState(false);
+  const [sideBarOpen, setSideBarOpen] = useState(false);
   return (
     <>
       <SweetAlert
@@ -47,11 +48,47 @@ const Header = () => {
       <div className="app_header h-24 flex items-center justify-between px-24 sticky flex-col md:flex-row">
         <img
           src="/media/logo"
-          className="h-16 cursor-pointer"
+          className="h-16 my-4 md:my-0 cursor-pointer"
           alt="brand_logo"
         />
 
-        <div className="menu_items flex items-center justify-between flex-col md:flex-row">
+        <button
+          onClick={() => {
+            if (sideBarOpen != true) {
+              setSideBarOpen(true);
+            } else {
+              setSideBarOpen(false);
+            }
+          }}
+          className="block md:hidden absolute right-4 top-8 text-white"
+        >
+          <div
+            style={{
+              width: "35px",
+              height: "5px",
+              backgroundColor: "white",
+              margin: "6px 0",
+            }}
+          ></div>
+          <div
+            style={{
+              width: "35px",
+              height: "5px",
+              backgroundColor: "white",
+              margin: "6px 0",
+            }}
+          ></div>
+          <div
+            style={{
+              width: "35px",
+              height: "5px",
+              backgroundColor: "white",
+              margin: "6px 0",
+            }}
+          ></div>
+        </button>
+
+        <div className="menu_items items-center justify-between hidden md:flex">
           <a className="cursor-pointer px-2">ABOUT</a>
           <a className="cursor-pointer px-2">FAQ</a>
           <a className="cursor-pointer px-2">TERMS AND CONDITIONS</a>
@@ -80,6 +117,45 @@ const Header = () => {
             </div>
           )}
         </div>
+
+        {sideBarOpen && (
+          <div style={{ background: "#1d2228" }} className="py-14">
+            <div className="menu_items items-center justify-between flex-col mx-auto">
+              <div className="flex flex-col text-center">
+                <a className="cursor-pointer my-2">ABOUT</a>
+                <a className="cursor-pointer my-2">FAQ</a>
+                <a className="cursor-pointer my-2">TERMS AND CONDITIONS</a>
+              </div>
+              {!userAccount ? (
+                <button
+                  className="login_button"
+                  onClick={() => {
+                    setUserLoginPopup(true);
+                  }}
+                >
+                  LOGIN
+                </button>
+              ) : (
+                <div className="flex-col text-center items-center flex">
+                  <div className="user_account_name my-2 mx-4">
+                    {userAccount}
+                  </div>
+                  <button
+                    className="login_button mx-4 my-1 my-2"
+                    onClick={() => {
+                      if (typeof window !== undefined) {
+                        window.location.href =
+                          "https://all-access.wax.io/logout";
+                      }
+                    }}
+                  >
+                    Logout
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
