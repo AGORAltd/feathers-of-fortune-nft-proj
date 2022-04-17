56d129dd602bbea191be1fd974364adc976a9733
Tushar <tusharkaramchandain25@gmail.com>
2022-04-17 20:24:55 +0530

    added out folder

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ["ipfs.atomichub.io"] },
+  images: { loader: "imgix", path: "", domains: ["ipfs.atomichub.io"] },
+
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/ended": { page: "/ended" },
+      "/ending-soon": { page: "/ending-soon" },
+      "/new": { page: "/new" },
+    };
+  },
 };
 
 module.exports = nextConfig;
