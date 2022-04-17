2d354bf4c6f57fb9cb12cfcc3880911303bc25c1
Tushar <tusharkaramchandain25@gmail.com>
2022-04-18 01:15:53 +0530

    added changes to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { loader: "imgix", path: "", domains: ["ipfs.atomichub.io"] },
+  images: { domains: ["ipfs.atomichub.io"] },
 
   exportPathMap: async function (
     defaultPathMap,
