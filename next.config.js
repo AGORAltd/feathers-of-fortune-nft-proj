38e9548e32220414ec927e16020d89b7ab0c5494
Tushar <tusharkaramchandain25@gmail.com>
2022-04-19 14:50:11 +0530

    added changes to next config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/atomicassets/v1/assets/:path*",
+        source: "https://wax.api.atomicassets.io/atomicassets/v1/assets/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
