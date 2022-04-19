8796231209ebbb70dffa677003b53be3ec058696
Tushar <tusharkaramchandain25@gmail.com>
2022-04-19 14:58:05 +0530

    added vercel

--- next.config.js
+++ next.config.js
@@ -3,28 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: { domains: ["ipfs.atomichub.io"] },
 
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/atomicassets/v1/assets/",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
-  },
-
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
