4f6eebcfc3d2906438366f1de8291760da3f1612
Tushar <tusharkaramchandain25@gmail.com>
2022-05-19 21:22:21 +0530

    success

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { loader: "akamai", path: "", domains: ["ipfs.atomichub.io"] },
+  images: { domains: ["ipfs.atomichub.io"] },
   exportPathMap: async function (
     defaultPathMap,
     { dev, dir, outDir, distDir, buildId }
