5d0b06ed1945f546e4e8129a00c14c49706e4b2c
Tushar <tusharkaramchandain25@gmail.com>
2022-05-29 15:19:49 +0530

    added changes

--- components/features/NftSection/components/NftCard.js
+++ components/features/NftSection/components/NftCard.js
@@ -377,14 +377,11 @@ const GetWinnerWhenExpired = ({
 
       let campaignDataOnExpire = await response.data?.rows;
 
-      const currentWinner = campaignDataOnExpire
-        ?.find((item) => {
-          return item?.asset_id == assetIdToFindWith;
-        })
-        .then(() => {
-          setCurrentWinnerUser(() => currentWinner?.winner);
-          console.log(currentWinnerUser);
-        });
+      const currentWinner = await campaignDataOnExpire?.find((item) => {
+        return item?.asset_id == assetIdToFindWith;
+      });
+
+      setCurrentWinnerUser(() => currentWinner?.winner);
     };
   }, [currentWinnerUser]);
 
