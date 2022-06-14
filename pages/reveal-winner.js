d0df7b580b7c5c3469de7585f617cd486f51da85
Tushar <tusharkaramchandain25@gmail.com>
2022-06-14 22:35:24 +0530

    added changes

--- pages/reveal-winner.js
+++ pages/reveal-winner.js
@@ -65,14 +65,13 @@ export async function getStaticProps() {
     try {
       responseFromPost.data?.rows.forEach((runningCampaigns, index) => {
         if (
-          (runningCampaigns?.asset_ids?.length > 0 &&
-            snapshot
-              .child("campaigns")
-              .hasChild(runningCampaigns?.asset_ids[0] + index) == false) ||
-          (runningCampaigns?.asset_ids?.length > 0 &&
-            snapshot
-              .child("campaigns")
-              .hasChild(runningCampaigns?.asset_ids[0] + index - 1) == false)
+          runningCampaigns?.asset_ids?.length > 0 &&
+          snapshot
+            .child("campaigns")
+            .hasChild(runningCampaigns?.asset_ids[0] + index) == false &&
+          snapshot
+            .child("campaigns")
+            .hasChild(runningCampaigns?.asset_ids[0] + index - 1) == false
         ) {
           axios
             .get(
