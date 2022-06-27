8fabc43dcf5607a938e1a1d8f5756b8dc24ff7f6
Tushar <tusharkaramchandain25@gmail.com>
2022-06-28 02:06:40 +0530

    added changes

--- components/features/NftSection/components/NftCard.js
+++ components/features/NftSection/components/NftCard.js
@@ -38,19 +38,10 @@ const NftCard = ({
     setUserLoginProvider,
   } = useContext(NftContext);
 
-  useEffect(() => {
-    if (window.location.pathname != "/reveal-winner") {
-      updateTimeToShow(finalUTCEpochTimeInMilliSec);
-    } else {
-      setTimeToShow("Reveal Winner");
-    }
-  }, []);
-
   const [showAlert, setShowAlert] = useState(false);
   const [showTransactionMessage, setShowTransactionMessage] = useState(false);
   const [showWinner, setShowWinner] = useState(false);
   const [showErrorMessage, setShowErrorMessage] = useState(false);
-  const [timeToShow, setTimeToShow] = useState("");
   const [openModal, setOpenModal] = useState(true);
   const [timeCountDown, setTimeCountDown] = useState(15);
   const [userLoginPopup, setUserLoginPopup] = useState(false);
@@ -67,26 +58,37 @@ const NftCard = ({
   }, [transactionId]);
 
   const updateTimeToShow = (finalUTCEpochTimeInMilliSec) => {
-    let interval = setInterval(() => {
-      let distance = finalUTCEpochTimeInMilliSec - new Date(Date.now());
+    let distance = finalUTCEpochTimeInMilliSec - +new Date(Date.now());
 
-      if (distance > 0) {
-        let hours = Math.floor((distance / 1000 / 60 / 60) % 24);
-        let minutes = Math.floor((distance / 1000 / 60) % 60);
-        let seconds = Math.floor((distance / 1000) % 60);
+    let timeLeft;
 
-        setTimeToShow(
-          `${hours}:${minutes < 10 ? "0" + minutes : minutes}:${
-            seconds < 10 ? "0" + seconds : seconds
-          }`
-        );
-      }
+    if (distance > 0) {
+      let hours = Math.floor((distance / 1000 / 60 / 60) % 24);
+      let minutes = Math.floor((distance / 1000 / 60) % 60);
+      let seconds = Math.floor((distance / 1000) % 60);
 
-      if (distance <= 0) {
-        clearInterval(interval);
-      }
-    }, 1000);
+      timeLeft = `${hours}:${minutes < 10 ? "0" + minutes : minutes}:${
+        seconds < 10 ? "0" + seconds : seconds
+      }`;
+    }
+
+    return timeLeft;
   };
+
+  const [timeToShow, setTimeToShow] = useState(
+    updateTimeToShow(finalUTCEpochTimeInMilliSec)
+  );
+
+  useEffect(() => {
+    if (window.location.pathname != "/reveal-winner") {
+      const timer = setTimeout(() => {
+        setTimeToShow(updateTimeToShow(finalUTCEpochTimeInMilliSec));
+      }, 1000);
+      return () => clearTimeout(timer);
+    } else {
+      setTimeToShow("Reveal Winner");
+    }
+  }, [timeToShow]);
 
   return (
     <>
